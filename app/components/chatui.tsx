d33990190914cb0686b5d277d739458da56198af
inpaceee <116308003+inpaceee@users.noreply.github.com>
2024-07-15 11:57:31 +0200

    feat: improves visuals

---
+++ app/components/chatui.tsx
@@ -0,0 +1,60 @@
+import type { MetaFunction } from "@remix-run/node";
+import { useState } from "react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function ChatUI() {
+  const [responses, setResponses] = useState<string[]>([]);
+  const [text, setText] = useState<string>("");
+
+  const handleClick = async () => {
+    // console.log("...sending text", text);
+    const response = await fetch("http://localhost:3000/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input: text }),
+    });
+    const result = await response.json();
+    setResponses((old) => [...old, result.response]);
+    setText("");
+  };
+
+  return (
+    <div className="font-sans p-4">
+      <div className="chat chat-start">
+        <div className="chat-bubble">Welcome to George AI</div>
+      </div>
+      {responses.map((response, index) => (
+        <div key={index} className="chat chat-start">
+          <div className="chat-bubble">{response}</div>
+        </div>
+      ))}
+      <div className="chat chat-end">
+        <div className="chat-bubble">
+          <textarea
+            placeholder="Enter your message"
+            value={text}
+            onChange={(text) => setText(text.target.value)}
+            className="textarea textarea-bordered"
+          ></textarea>
+          <br />
+          <button
+            onClick={() => {
+              handleClick();
+            }}
+            className="btn btn-sm"
+          >
+            Send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useState } from "react";
+import ChatUI from "../components/chatui";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,50 +9,10 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const [responses, setResponses] = useState<string[]>([]);
-  const [text, setText] = useState<string>("");
-
-  const handleClick = async () => {
-    console.log("...sending text", text);
-    const response = await fetch("http://localhost:3000/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ input: text }),
-    });
-    const result = await response.json();
-    setResponses((old) => [...old, result.response]);
-    setText("");
-  };
-
   return (
-    <div className="font-sans p-4">
-      <div className="chat chat-start">
-        <div className="chat-bubble">Welcome to George AI</div>
-      </div>
-      {responses.map((response, index) => (
-        <div key={index} className="chat chat-start">
-          <div className="chat-bubble">{response}</div>
-        </div>
-      ))}
-      <div className="chat chat-end">
-        <div className="chat-bubble">
-          <textarea
-            placeholder="Enter your message"
-            value={text}
-            onChange={(text) => setText(text.target.value)}
-          ></textarea>
-          <button
-            onClick={() => {
-              handleClick();
-            }}
-            className="btn"
-          >
-            Send
-          </button>
-        </div>
-      </div>
+    <div>
+      <h1>Welcome to Remix</h1>
+      <ChatUI />
     </div>
   );
 }
